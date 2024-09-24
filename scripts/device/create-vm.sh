d59490bf524c2432860df2179160a448c82c7b1e
sserafin <sserafin@redhat.com>
2024-09-24 11:19:38 +0200

    Update script

--- scripts/device/create-vm.sh
+++ scripts/device/create-vm.sh
@@ -1,16 +1,17 @@
 #Export QUAY_USER and QUAY_PASSWORD with your quay.io account
 #Pre-requisite. Have Containerfile in /home/kni
 
-git clone git@github.com:flightctl/flightctl-demos.git ../
+git clone git@github.com:flightctl/flightctl-demos.git ~/
 openssl ecparam -name secp521r1 -genkey -noout -out myeckey.pem
 openssl req -new -sha256 -key myeckey.pem -out mycsr.csr
-./bin/flightctl csr-generate mycsr.csr -e 604800 -n mycsr -o myoutputfile -y
-./bin/flightctl	apply -f myoutputfile
-./bin/flightctl approve csr/mycsr
+./flightctl/bin/flightctl csr-generate mycsr.csr -e 604800 -n mycsr -o myoutputfile -y
+./flightctl/bin/flightctl       apply -f myoutputfile
+./flightctl/bin/flightctl approve csr/mycsr
 sudo podman login quay.io -u ${QUAY_USER} -p ${QUAY_PASSWORD}
-./bin/flightctl enrollmentconfig mycsr --private-key myeckey.pem > agentconfig.yaml
-cp agentconfig.yaml ../flightctl-demo/images/bootc/centos-bootc/
-cp Containerfile ../flightctl-demo/images/bootc/centos-bootc/
+./flightctl/bin/flightctl enrollmentconfig mycsr --private-key myeckey.pem > agentconfig.yaml
+cp agentconfig.yaml ~/flightctl-demo/images/bootc/centos-bootc/
+cp Containerfile ~/flightctl-demo/images/bootc/centos-bootc/
+cd ~/flightctl-demos
 make bootc-centos QUAYUSER=${QUAY_USER} # the quay.io repository must be set to public
 make qcow2-bootc QUAYUSER=${QUAY_USER} flavor=centos
 
@@ -24,11 +25,11 @@ export VMWAIT=0
 sudo cp ~/flightctl-demos/output/qcow2/disk.qcow2 $VMDISK
 sudo chown libvirt:libvirt $VMDISK 2>/dev/null || true
 sudo virt-install --name $VMNAME \
-   	 --tpm backend.type=emulator,backend.version=2.0,model=tpm-tis \
-   				   --vcpus $VMCPUS \
-   				   --memory $VMRAM \
-   				   --import --disk $VMDISK,format=qcow2 \
-   				   --os-variant fedora-eln  \
-   				   --autoconsole text \
-   				   --wait $VMWAIT \
-   				   --transient || true
+         --tpm backend.type=emulator,backend.version=2.0,model=tpm-tis \
+                                   --vcpus $VMCPUS \
+                                   --memory $VMRAM \
+                                   --import --disk $VMDISK,format=qcow2 \
+                                   --os-variant fedora-eln  \
+                                   --autoconsole text \
+                                   --wait $VMWAIT \
+                                   --transient || true
